cc8e58e4dd6562616167e3e24876c69f8591d7e5
Viktor Obedkov <obedkov.2018@yandex.ru>
2023-10-15 09:55:33 +0300

    Добавил template, функции подназвание createCard, deleteCard и добавил вывести на карточки на страницу foreach

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const templateCards = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const placesItem = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(card) {
+    const cardElement = templateCards.querySelector('.card').cloneNode(true);
+    cardElement.querySelector('.card__image').alt = card.name;
+    cardElement.querySelector('.card__image').src = card.link;
+    cardElement.querySelector('.card__title').textContent = card.name;
+    return cardElement;
+}
 
 // @todo: Функция удаления карточки
+function deleteCard(event) {
+    const card = event.target.closest('.places__item');
+    card.remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function (card) {
+    const newCardElement = createCard(card);
+    const buttonDelete = newCardElement.querySelector('.card__delete-button');
+    buttonDelete.addEventListener('click', deleteCard);
+    placesItem.append(newCardElement);
+});
