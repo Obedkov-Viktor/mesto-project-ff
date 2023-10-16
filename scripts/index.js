7f4fb73ed10d3a739efc6df5d812780a46670c49
Viktor Obedkov <obedkov.2018@yandex.ru>
2023-10-16 10:23:34 +0300

    Исправил функция удаления

--- scripts/index.js
+++ scripts/index.js
@@ -5,24 +5,28 @@ const templateCards = document.querySelector('#card-template').content;
 const placesItem = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-function createCard(card) {
+function createCard(card, deleteCard) {
     const cardElement = templateCards.querySelector('.card').cloneNode(true);
+    const buttonDelete = cardElement.querySelector('.card__delete-button');
     cardElement.querySelector('.card__image').alt = card.name;
     cardElement.querySelector('.card__image').src = card.link;
     cardElement.querySelector('.card__title').textContent = card.name;
+
+    // @todo: удаления карточки
+    buttonDelete.addEventListener('click', () => {
+        deleteCard(cardElement)
+    });
+
     return cardElement;
 }
 
 // @todo: Функция удаления карточки
-function deleteCard(event) {
-    const card = event.target.closest('.places__item');
-    card.remove();
+const deleteCard = (cardElement) => {
+    cardElement.remove();
 }
 
 // @todo: Вывести карточки на страницу
 initialCards.forEach(function (card) {
-    const newCardElement = createCard(card);
-    const buttonDelete = newCardElement.querySelector('.card__delete-button');
-    buttonDelete.addEventListener('click', deleteCard);
+    const newCardElement = createCard(card, deleteCard);
     placesItem.append(newCardElement);
 });
