016a55818966a9db10e346e12088343c057ab3c1
Viktor Obedkov <obedkov.2018@yandex.ru>
2023-11-14 22:12:14 +0300

    спринт 6 =  2 часть

--- src/components/modal.js
+++ src/components/modal.js
@@ -1,18 +1,50 @@
-import {handleEscClose} from "../index";
+function handleEscClose(event) {
+    if (event.key === 'Escape') {
+        const popup = document.querySelector('.popup_is-opened');
+        closeModal(popup);
+    }
+}
 
+// @todo Функция закрытия попапа по клику на оверлей
+function handleCloseOverlayClick(event) {
+    if(event.target.classList.contains('popup')){// Проверяем, что клик был именно по оверлею
+        const popup = document.querySelector('.popup_is-opened');
+        closeModal(popup);
+    }
+}
+
+//@todo Функция открытыя попапа
 function openModal(popup) {
     const closeButton = popup.querySelector('.popup__close');
     popup.classList.add('popup_is-opened');
     document.addEventListener('keydown', handleEscClose);
-    closeButton.addEventListener('click', () => {
-        closeModal(popup);
-    });
-
+    closeButton.addEventListener('click', handleCloseButtonClick);
+    popup.addEventListener('click', handleCloseOverlayClick);
+}
+//@todo Функция закрывает кнопки нажатие
+function handleCloseButtonClick() {
+    const popup = this.closest('.popup');
+    closeModal(popup);
 }
 
+//@todo Функция закрытия попапа
 function closeModal(popup) {
     popup.classList.remove('popup_is-opened');
     document.removeEventListener('keydown', handleEscClose);
+
+    popup.removeEventListener('click', handleCloseOverlayClick);
 }
 
-export {openModal, closeModal};
+//@todo Создать функции openImagePopup, которая будет открывать попап с картинкой:
+function openImagePopup(imageLink, caption) {
+    const popupImage = document.querySelector('.popup_type_image');
+    const popupImageElement = popupImage.querySelector('.popup__image');
+    const popupImageCaption = popupImage.querySelector('.popup__caption');
+
+    popupImageElement.src = imageLink;
+    popupImageElement.alt = caption;
+    popupImageCaption.textContent = caption;
+    openModal(popupImage);
+}
+
+export {openModal, closeModal, openImagePopup};
--- src/index.js
+++ src/index.js
@@ -1,88 +1,42 @@
 import './styles/index.css';
-import {initialCards} from "./components/cards";
+import {initialCards, createCard} from "./components/cards";
 import {openModal, closeModal} from "./components/modal";
 
-// @todo: Темплейт карточки
-const templateCards = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const placesItem = document.querySelector('.places__list');
 
 // @todo: редактировать  тип всплывающего окна
-const editPopup = document.querySelector('.popup_type_edit');
+const popupProfileOpenButton = document.querySelector('.popup_type_edit');
 
 // @todo: кнопки профиль  редактировать тип всплывающего окна
 const editButton = document.querySelector('.profile__edit-button');
 
 // @todo: кнопки добавить card тип всплывающего окна
-const newCard = document.querySelector('.popup_type_new-card');
+const popupAddCard = document.querySelector('.popup_type_new-card');
 
 // @todo: кнопки профиль  добавить тип всплывающего окна
 const addButton = document.querySelector('.profile__add-button');
 
-// @todo: закрывать тип всплывающего окна
-const closeButton = document.querySelector('.popup__close');
-
 // @todo: querySelector forms
-const formElement = document.querySelector(".popup__form");
+const formProfile = document.querySelector('form[name="edit-profile"]');
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_description');
 const profileName = document.querySelector('.profile__title');
 const profileDescription = document.querySelector('.profile__description');
 
-editButton.addEventListener('click', () => openModal(editPopup));
-addButton.addEventListener('click', () => openModal(newCard));
-
-// @todo: Функция создания карточки
-function createCard(card, deleteCard, handleLike) {
-    const cardElement = templateCards.querySelector('.card').cloneNode(true);
-    const buttonDelete = cardElement.querySelector('.card__delete-button');
-    const buttonLike = cardElement.querySelector('.card__like-button');
-
-    cardElement.querySelector('.card__image').alt = card.name;
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__title').textContent = card.name;
-
-    // @todo: обрабочтик события клик на изображеение
-    cardElement.querySelector('.card__image').addEventListener('click', () => {
-        openImagePopup(card.link, card.name)
-    });
-
-    // @todo: удаления карточки
-    buttonDelete.addEventListener('click', () => {
-        deleteCard(cardElement)
-    });
-
-    // @todo: лайк карточки
-    buttonLike.addEventListener('click', () => {
-        handleLike(buttonLike);
-    });
-
-    return cardElement;
-}
-
-//@todo Создайте функцию openImagePopup, которая будет открывать попап с картинкой:
-function openImagePopup(imageLink, caption) {
-    const popupImage = document.querySelector('.popup_type_image');
-    const popupImageElement = popupImage.querySelector('.popup__image');
-    const popupImageCaption = popupImage.querySelector('.popup__caption');
-
-    popupImageElement.src = imageLink;
-    popupImageElement.alt = caption;
-    popupImageCaption.textContent = caption;
-    openModal(popupImage);
-}
+//@todo Обработчик события submit для формы создания новой карточки.
+const newCardForm = document.querySelector('.popup_type_new-card .popup__form');
+const cardContainer = document.querySelector('.places__list');
+editButton.addEventListener('click', () => openModal(popupProfileOpenButton));
+addButton.addEventListener('click', () => openModal(popupAddCard));
 
 // @todo: Функция лайк карточки
 function handleLike(button) {
     button.classList.toggle('card__like-button_is-active')
 }
 
-//@todo Обработчик события submit для формы создания новой карточки.
-const newCardForm = document.querySelector('.popup_type_new-card .popup__form');
-const cardContainer = document.querySelector('.places__list');
-
-// Обработчик события submit для формы создания новой карточки
+// @todo: Обработчик события submit для формы создания новой карточки
 function handleNewCardSubmit(event) {
     event.preventDefault();
 
@@ -120,12 +74,7 @@ initialCards.forEach(function (card) {
 });
 
 // @todo Закрытие попапа кликом на "Esc"
-function handleEscClose(event) {
-    if (event.key === 'Escape') {
-        const popup = document.querySelector('.popup_is-opened');
-        closeModal(popup);
-    }
-}
+
 
 // @todo: При открытии формы поля «Имя» и «О себе»
 nameInput.value = profileName.textContent;
@@ -143,7 +92,4 @@ function handleFormSubmit(evt) {
     closeModal(popup);
 }
 
-formElement.addEventListener('submit', handleFormSubmit);
-
-// @todo: экспортировать
-export {handleEscClose};
+formProfile.addEventListener('submit', handleFormSubmit);
